a514ec6a2dae8a423cf86474f03c62cd30c1e43e
Israel Gomez <2023371154@uteq.edu.mx>
2026-04-15 13:57:06 -0600

    Update index.js

--- abejanet-backend/index.js
+++ abejanet-backend/index.js
@@ -16,6 +16,7 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: process.env.DB_HOST === 'localhost' ? false : { rejectUnauthorized: false },
 });
 
 // ==========================================
